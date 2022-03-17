2da43d7d1302089fe9ae6846579212808d42f360
Andrey <priv1984@yandex.ru>
2022-03-17 22:50:26 +0700

    create portfolio #1

--- src/blockСomponents/Home/Home.js
+++ src/blockСomponents/Home/Home.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 let Home = (props) => {
+
+
+
+
+   const [ten, setTen]=useState(0);
+
+    let utc = Math.round(new Date().getTime() / 1.0);
+
+    setTimeout(() => {
+       setTen(Math.round(utc*0.00266 - 3511351,62) + 3531865500);
+    }, 100);
+
     return <>
         <div>Привет, Это Home.js</div>
-        <div> Привет. Меня зовут Андрей. Я начинающий web-программист. HTML, css, JavaScript, ReactJS - это то, с чем я
-            работаю.
-        </div>
+        <div>{ten}</div>
     </>
 }
 
